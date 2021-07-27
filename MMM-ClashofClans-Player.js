d5b226cf6a88ebb54e28464893d4c3b9f0b7b2a8
Hauke Marquard <haukemarquard@Mac-mini-von-Hauke.fritz.box>
2021-07-27 15:04:52 +0200

    Ausbesserungen

--- MMM-ClashofClans-Player.js
+++ MMM-ClashofClans-Player.js
@@ -1,20 +1,20 @@
 Module.register("MMM-ClashofClans-Player", {
     defaults: {
         updateInterval: 30000,
-        playerTag: YQLQ902Y, //Without the Hashtag
+        playerTag: "YQLQ902Y", //Without the Hashtag
     },
 
     start: function () {
-        Log.info(`Starting Module: ${this.name}`)
+        Log.info(`Starting Module: ${this.name}`);
 
     },
 
     getDom: function () {
-        var wrapper = document.createElement("div")
-        var compliment = document.createElement("span")
-        compliment.appendChild(document.createTextNode(this.config.playerTag))
-        wrapper.appendChild(compliment)
+        var wrapper = document.createElement("div");
+        var compliment = document.createElement("span");
+        compliment.appendChild(document.createTextNode("Test"));
+        wrapper.appendChild(compliment);
 
-        return wrapper
+        return wrapper;
     }
 })
