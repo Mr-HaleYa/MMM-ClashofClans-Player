d8d6fa0dd37caa49dfd5858ab80e1ae8714c1ede
Hauke Marquard <haukemarquard@Mac-mini-von-Hauke.fritz.box>
2021-07-27 14:50:24 +0200

    Testing by showing the PlayerID

---
+++ MMM-ClashofClans-Player.js
@@ -0,0 +1,20 @@
+Module.register("MMM-ClashofClans-Player", {
+    defaults: {
+        updateInterval: 30000,
+        playerTag: YQLQ902Y, //Without the Hashtag
+    },
+
+    start: function () {
+        Log.info(`Starting Module: ${this.name}`)
+
+    },
+
+    getDom: function () {
+        var wrapper = document.createElement("div")
+        var compliment = document.createElement("span")
+        compliment.appendChild(document.createTextNode(this.config.playerTag))
+        wrapper.appendChild(compliment)
+
+        return wrapper
+    }
+})
