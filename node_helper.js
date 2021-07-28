f6ffac648f99697240a9973f166028c1bf7c112f
Hauke Marquard <haukemarquard@Mac-mini-von-Hauke.fritz.box>
2021-07-28 13:52:51 +0200

    Jetzt?

--- node_helper.js
+++ node_helper.js
@@ -12,10 +12,27 @@ module.exports = NodeHelper.create({
     getPlayerStats: function(payload) {
         //var that = this
         this.url = 'https://api.clashofclans.com/v1/players/%23' + payload
+        this.token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjMyYjcyMWQxLTYwNzAtNGI0YS04OTg0LTM2ZGE3ZTRhMzg3ZCIsImlhdCI6MTYyNzM4NzE1OCwic3ViIjoiZGV2ZWxvcGVyLzdhY2MxMWJkLTg0ZmQtNjNjZS1iN2QyLTY0Y2NhZTc1NWIxMCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjc5LjIwNy4xNTcuMTk0Il0sInR5cGUiOiJjbGllbnQifV19.I6AylkHJQmKiAtWUj-AmmAOruuKv0gk6J4BKk0VWjuX0fa1l7S8JWtJDJOnrt4xbKcOQn6ij0kdeKkyicdKTaw'
 
-        request({
+        this.object = {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer ' + this.token
+            }
+        }
+
+        fetch(this.url, this.object)
+            .then(function (res) {
+                this.sendSocketNotification('GOT-PLAYER-STATS', res.json())
+
+            })
+
+        /*request({
             url: this.url,
-            method: 'GET'
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer ' + this.token
+            }
         }, (error, response, body) => {
 
             if(!error && response.statusCode == 200){
@@ -26,7 +43,7 @@ module.exports = NodeHelper.create({
                 this.playerName = 'Error'
             }
             this.sendSocketNotification('GOT-PLAYER-STATS', {'name': this.PlayerName, 'trophies': this.trophies})
-        })
+        })*/
     },
 
     socketNotificationReceived: function(notification, payload) {
